849dcd863ce8275fc7f90dfb5056506dc446751e
Developerteck <66458929+Developerteck@users.noreply.github.com>
2022-07-23 20:18:27 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,5 +1,5 @@
 
-import { useState } from "react"
+import React, { useState } from "react"
 
 export default function Selection({applyColor}){
   const [color, setColor] = useState({background:""})
