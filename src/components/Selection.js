5db43132ade33d9bf86c0fa67df7ef615201a8ef
Developerteck <66458929+Developerteck@users.noreply.github.com>
2022-07-23 20:04:28 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,11 @@
 
+import { useState } from "react"
+
+export default function Selection({applyColor}){
+  const [color, setColor] = useState({background:""})
+  return (
+    <div onClick={()=>{applyColor(setColor)}} style={color} className="fix-box">
+      <h2 className="subheading">Selection</h2>
+    </div>
+  )
+}
